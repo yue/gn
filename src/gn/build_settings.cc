e96b1bb71f81a65b00ad1736154aa71b12744a08
Cheng Zhao <zcbenz@gmail.com>
2017-09-29 19:30:07 +0900

    Add use_chromium_config setting for .gn

--- src/gn/build_settings.cc
+++ src/gn/build_settings.cc
@@ -7,17 +7,24 @@
 #include <utility>
 
 #include "base/files/file_util.h"
+#include "base/strings/string_util.h"
 #include "gn/filesystem_utils.h"
+#include "util/exe_path.h"
 
-BuildSettings::BuildSettings() = default;
+BuildSettings::BuildSettings()
+    : chromium_config_path_(GetExePath().DirName()),
+      chromium_config_path_utf8_(FilePathToUTF8(chromium_config_path_)) {}
 
 BuildSettings::BuildSettings(const BuildSettings& other)
     : dotfile_name_(other.dotfile_name_),
       root_path_(other.root_path_),
       root_path_utf8_(other.root_path_utf8_),
       secondary_source_path_(other.secondary_source_path_),
+      chromium_config_path_(other.chromium_config_path_),
+      chromium_config_path_utf8_(other.chromium_config_path_utf8_),
       python_path_(other.python_path_),
       ninja_required_version_(other.ninja_required_version_),
+      use_chromium_config_(other.use_chromium_config_),
       build_config_file_(other.build_config_file_),
       arg_file_template_path_(other.arg_file_template_path_),
       build_dir_(other.build_dir_),
@@ -67,6 +74,53 @@ base::FilePath BuildSettings::GetFullPathSecondary(const std::string& path,
                                                    bool as_file) const {
   return ResolvePath(path, as_file, secondary_source_path_)
       .NormalizePathSeparatorsTo('/');
+}
+
+base::FilePath BuildSettings::GetFullPathChromium(
+    const SourceFile& file) const {
+  return file.Resolve(chromium_config_path_).NormalizePathSeparatorsTo('/');
+}
+
+base::FilePath BuildSettings::GetFullPathChromium(
+    const SourceDir& dir) const {
+  return dir.Resolve(chromium_config_path_).NormalizePathSeparatorsTo('/');
+}
+
+base::FilePath BuildSettings::GetFullPathChromium(const std::string& path,
+                                                  bool as_file) const {
+  return ResolvePath(path, as_file, chromium_config_path_)
+      .NormalizePathSeparatorsTo('/');
+}
+
+bool BuildSettings::IsChromiumPath(const std::string& path) const {
+  return use_chromium_config() &&
+      (base::StartsWith(path, "//build/", base::CompareCase::SENSITIVE) ||
+       base::StartsWith(path, "//buildtools/", base::CompareCase::SENSITIVE) ||
+       base::StartsWith(path, "//testing/", base::CompareCase::SENSITIVE) ||
+       base::StartsWith(path, "//tools/", base::CompareCase::SENSITIVE) ||
+       base::StartsWith(path, "//third_party/catapult/", base::CompareCase::SENSITIVE) ||
+       base::StartsWith(path, "//third_party/googletest/", base::CompareCase::SENSITIVE) ||
+       base::StartsWith(path, "//third_party/libc++/", base::CompareCase::SENSITIVE) ||
+       base::StartsWith(path, "//third_party/libc++abi/", base::CompareCase::SENSITIVE) ||
+       base::StartsWith(path, "//third_party/libunwind/", base::CompareCase::SENSITIVE) ||
+       base::StartsWith(path, "//third_party/llvm-build/", base::CompareCase::SENSITIVE));
+}
+
+std::string BuildSettings::TranslateChromiumFullPath(
+    const base::FilePath& path) const {
+  std::string real_path = FilePathToUTF8(path);
+  std::string relative_path_to_real_path;
+  if (MakeAbsolutePathRelativeIfPossible(root_path_utf8(),
+                                         real_path,
+                                         &relative_path_to_real_path)) {
+    return relative_path_to_real_path;
+  } else {
+#if defined(OS_WIN)
+    return "/" + real_path;
+#else
+    return real_path;
+#endif
+  }
 }
 
 void BuildSettings::ItemDefined(std::unique_ptr<Item> item) const {
